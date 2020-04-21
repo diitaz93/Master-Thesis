2cf85af9504b76e27613de04f8316d017b0dbc45
diitaz93 <juansdb93@gmail.com>
2020-04-21 17:42:48 +0200

    turned the notebook into a script to be run on the docker container

---
+++ test.py
@@ -0,0 +1,291 @@
+from __future__ import division
+from __future__ import print_function
+from operator import itemgetter
+from itertools import combinations
+import time
+import os
+import tensorflow as tf
+import numpy as np
+import networkx as nx
+import scipy.sparse as sp
+from sklearn import metrics
+from decagon.deep.optimizer import DecagonOptimizer
+from decagon.deep.model import DecagonModel
+from decagon.deep.minibatch import EdgeMinibatchIterator
+from decagon.utility import rank_metrics, preprocessing
+from data.utils_mod import *
+
+# Train on GPU
+os.environ["CUDA_DEVICE_ORDER"] = 'PCI_BUS_ID'
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
+###########################################################
+#
+# Functions
+#
+###########################################################
+def get_accuracy_scores(edges_pos, edges_neg, edge_type):
+    feed_dict.update({placeholders['dropout']: 0})
+    feed_dict.update({placeholders['batch_edge_type_idx']: minibatch.edge_type2idx[edge_type]})
+    feed_dict.update({placeholders['batch_row_edge_type']: edge_type[0]})
+    feed_dict.update({placeholders['batch_col_edge_type']: edge_type[1]})
+    rec = sess.run(opt.predictions, feed_dict=feed_dict)
+
+    def sigmoid(x):
+        return 1. / (1 + np.exp(-x))
+
+    # Predict on test set of edges
+    preds = []
+    actual = []
+    predicted = []
+    edge_ind = 0
+    for u, v in edges_pos[edge_type[:2]][edge_type[2]]:
+        score = sigmoid(rec[u, v])
+        preds.append(score)
+        assert adj_mats_orig[edge_type[:2]][edge_type[2]][u,v] == 1, 'Problem 1'
+
+        actual.append(edge_ind)
+        predicted.append((score, edge_ind))
+        edge_ind += 1
+
+    preds_neg = []
+    for u, v in edges_neg[edge_type[:2]][edge_type[2]]:
+        score = sigmoid(rec[u, v])
+        preds_neg.append(score)
+        assert adj_mats_orig[edge_type[:2]][edge_type[2]][u,v] == 0, 'Problem 0'
+
+        predicted.append((score, edge_ind))
+        edge_ind += 1
+
+    preds_all = np.hstack([preds, preds_neg])
+    preds_all = np.nan_to_num(preds_all)
+    labels_all = np.hstack([np.ones(len(preds)), np.zeros(len(preds_neg))])
+    predicted = list(zip(*sorted(predicted, reverse=True, key=itemgetter(0))))[1]
+
+    roc_sc = metrics.roc_auc_score(labels_all, preds_all)
+    aupr_sc = metrics.average_precision_score(labels_all, preds_all)
+    apk_sc = rank_metrics.apk(actual, predicted, k=50)
+
+    return roc_sc, aupr_sc, apk_sc
+
+def construct_placeholders(edge_types):
+    placeholders = {
+        'batch': tf.placeholder(tf.int32, name='batch'),
+        'batch_edge_type_idx': tf.placeholder(tf.int32, shape=(), name='batch_edge_type_idx'),
+        'batch_row_edge_type': tf.placeholder(tf.int32, shape=(), name='batch_row_edge_type'),
+        'batch_col_edge_type': tf.placeholder(tf.int32, shape=(), name='batch_col_edge_type'),
+        'degrees': tf.placeholder(tf.int32),
+        'dropout': tf.placeholder_with_default(0., shape=()),
+    }
+    placeholders.update({
+        'adj_mats_%d,%d,%d' % (i, j, k): tf.sparse_placeholder(tf.float32)
+        for i, j in edge_types for k in range(edge_types[i,j])})
+    placeholders.update({
+        'feat_%d' % i: tf.sparse_placeholder(tf.float32)
+        for i, _ in edge_types})
+    return placeholders
+
+###########################################################
+#
+# Load and preprocess data
+#
+###########################################################
+# Loading Gene data (PPI)
+ppi, gene2idx = load_ppi(fname='data/modif_data/ppi_mini.csv')
+ppi_adj = nx.adjacency_matrix(ppi)
+ppi_degrees = np.array(ppi_adj.sum(axis=0)).squeeze() 
+n_genes = ppi.number_of_nodes() # Number of genes (nodes)
+# Loading individual side effects
+stitch2se, semono2name, semono2idx = load_mono_se(fname='data/modif_data/mono_mini.csv')
+n_semono = len(semono2name)
+print('Number of individual side effects: ', n_semono)
+# Loading Target data (DTI)
+stitch2proteins = load_targets(fname='data/modif_data/target_mini.csv')
+# Loading Drug data (DDI)
+combo2stitch, combo2se, se2name, drug2idx = load_combo_se(fname='data/modif_data/combo_mini.csv')
+# Loading Side effect data (features)
+#stitch2se, se2name = load_mono_se(fname='polypharmacy/mono_mini.csv')
+n_drugs = len(drug2idx)
+print('Number of drugs: ', n_drugs)
+
+# Drug-traget adjacency matrix
+dti_adj = np.zeros([n_genes,n_drugs],dtype=int)
+for drug in drug2idx.keys():
+    for gene in stitch2proteins[drug]:
+        if gene==set():
+            continue
+        else:
+            idp = gene2idx[str(gene)]
+            idd = drug2idx[drug]
+            dti_adj[idp,idd] = 1  
+dti_adj = sp.csr_matrix(dti_adj)
+
+# DDi adjacency matrix
+ddi_adj_list = []
+for se in se2name.keys():
+    mat = np.zeros([n_drugs,n_drugs],dtype=int)
+    for pair in combo2se.keys():
+        if se in combo2se[pair]:
+            d1,d2 = combo2stitch[pair]
+            mat[drug2idx[d1],drug2idx[d2]] = 1
+    ddi_adj_list.append(sp.csr_matrix(mat))    
+ddi_degrees_list = [np.array(drug_adj.sum(axis=0)).squeeze() for drug_adj in ddi_adj_list]
+
+adj_mats_orig = {
+    (0, 0): [ppi_adj, ppi_adj.transpose(copy=True)],
+    (0, 1): [dti_adj],
+    (1, 0): [dti_adj.transpose(copy=True)],
+    (1, 1): ddi_adj_list + [x.transpose(copy=True) for x in ddi_adj_list],
+}
+degrees = {
+    0: [ppi_degrees, ppi_degrees],
+    1: ddi_degrees_list + ddi_degrees_list, 
+}
+
+# featureless (genes)
+gene_feat = sp.identity(n_genes)
+gene_nonzero_feat, gene_num_feat = gene_feat.shape
+gene_feat = preprocessing.sparse_to_tuple(gene_feat.tocoo())
+# features (drugs)
+oh_feat = np.zeros([n_drugs,n_semono], dtype=int)
+for drug in drug2idx.keys():
+    for se in stitch2se[drug]:
+        did = drug2idx[drug]
+        seid = semono2idx[se]
+        oh_feat[did,seid] = 1
+drug_feat = sp.csr_matrix(oh_feat)
+drug_nonzero_feat = n_semono
+drug_num_feat = n_semono
+drug_feat = preprocessing.sparse_to_tuple(drug_feat.tocoo())
+
+# data representation
+num_feat = {
+    0: gene_num_feat,
+    1: drug_num_feat,
+}
+nonzero_feat = {
+    0: gene_nonzero_feat,
+    1: drug_nonzero_feat,
+}
+feat = {
+    0: gene_feat,
+    1: drug_feat,
+}
+# Dictionary with the shape of all the matrices of the dictionary adj_mats_orig
+edge_type2dim = {k: [adj.shape for adj in adjs] for k, adjs in adj_mats_orig.items()}
+edge_type2decoder = {
+    (0, 0): 'bilinear',
+    (0, 1): 'bilinear',
+    (1, 0): 'bilinear',
+    (1, 1): 'dedicom',
+}
+#Dictionary with the number of matrices for each entry of adj_mats_orig
+edge_types = {k: len(v) for k, v in adj_mats_orig.items()}
+num_edge_types = sum(edge_types.values())
+
+###########################################################
+#
+# Settings and placeholders
+#
+###########################################################
+val_test_size = 0.05
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_integer('neg_sample_size', 1, 'Negative sample size.')
+flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
+flags.DEFINE_integer('epochs', 50, 'Number of epochs to train.')
+flags.DEFINE_integer('hidden1', 64, 'Number of units in hidden layer 1.')
+flags.DEFINE_integer('hidden2', 32, 'Number of units in hidden layer 2.')
+flags.DEFINE_float('weight_decay', 0, 'Weight for L2 loss on embedding matrix.')
+flags.DEFINE_float('dropout', 0.1, 'Dropout rate (1 - keep probability).')
+flags.DEFINE_float('max_margin', 0.1, 'Max margin parameter in hinge loss')
+flags.DEFINE_integer('batch_size', 512, 'minibatch size.')
+flags.DEFINE_boolean('bias', True, 'Bias term.')
+# Important -- Do not evaluate/print validation performance every iteration as it can take
+# substantial amount of time
+PRINT_PROGRESS_EVERY = 150
+print("Defining placeholders")
+placeholders = construct_placeholders(edge_types)
+
+###########################################################
+#
+# Create minibatch iterator, model and optimizer
+#
+###########################################################
+
+print("Create minibatch iterator")
+minibatch = EdgeMinibatchIterator(
+    adj_mats=adj_mats_orig,
+    feat=feat,
+    edge_types=edge_types,
+    batch_size=FLAGS.batch_size,
+    val_test_size=val_test_size
+)
+print("Create model")
+model = DecagonModel(
+    placeholders=placeholders,
+    num_feat=num_feat,
+    nonzero_feat=nonzero_feat,
+    edge_types=edge_types,
+    decoders=edge_type2decoder,
+)
+print("Create optimizer")
+with tf.name_scope('optimizer'):
+    opt = DecagonOptimizer(
+        embeddings=model.embeddings,
+        latent_inters=model.latent_inters,
+        latent_varies=model.latent_varies,
+        degrees=degrees,
+        edge_types=edge_types,
+        edge_type2dim=edge_type2dim,
+        placeholders=placeholders,
+        batch_size=FLAGS.batch_size,
+        margin=FLAGS.max_margin
+    )
+    
+print("Initialize session")
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+feed_dict = {}
+
+###########################################################
+#
+# Train model
+#
+###########################################################
+print("Train model")
+for epoch in range(FLAGS.epochs):
+
+    minibatch.shuffle()
+    itr = 0
+    while not minibatch.end():
+        # Construct feed dictionary
+        feed_dict = minibatch.next_minibatch_feed_dict(placeholders=placeholders)
+        feed_dict = minibatch.update_feed_dict(
+            feed_dict=feed_dict,
+            dropout=FLAGS.dropout,
+            placeholders=placeholders)
+
+        t = time.time()
+
+        # Training step: run single weight update
+        outs = sess.run([opt.opt_op, opt.cost, opt.batch_edge_type_idx], feed_dict=feed_dict)
+        train_cost = outs[1]
+        batch_edge_type = outs[2]
+
+        if itr % PRINT_PROGRESS_EVERY == 0:
+            val_auc, val_auprc, val_apk = get_accuracy_scores(
+                minibatch.val_edges, minibatch.val_edges_false,
+                minibatch.idx2edge_type[minibatch.current_edge_type_idx])
+
+            print("Epoch:", "%04d" % (epoch + 1), "Iter:", "%04d" % (itr + 1), "Edge:", "%04d" % batch_edge_type,
+                  "train_loss=", "{:.5f}".format(train_cost),
+                  "val_roc=", "{:.5f}".format(val_auc), "val_auprc=", "{:.5f}".format(val_auprc),
+                  "val_apk=", "{:.5f}".format(val_apk), "time=", "{:.5f}".format(time.time() - t))
+
+        itr += 1
+#sess.close()
+print("Optimization finished!")
+print("Total time: ", time.time()-t)
