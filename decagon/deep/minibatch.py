Came back to original approach in minibatch, but fixing data leakage

decagon/deep/minibatch.py
@@ -49,9 +49,7 @@ class EdgeMinibatchIterator(object):
         self.test_edges = {edge_type: [None]*n for edge_type, n in self.edge_types.items()}
         self.test_edges_false = {edge_type: [None]*n for edge_type, n in self.edge_types.items()}
         self.val_edges_false = {edge_type: [None]*n for edge_type, n in self.edge_types.items()}
-        ############
         self.train_edges_false = {edge_type: [None]*n for edge_type, n in self.edge_types.items()}
-        ############
         # Function to build test and val sets with val_test_size positive links
         self.adj_train = {edge_type: [None]*n for edge_type, n in self.edge_types.items()}
         for i, j in self.edge_types:
@@ -89,14 +87,14 @@ class EdgeMinibatchIterator(object):
         return np.any(rows_close)
     
 # Divides the data corresponding to a given edge in test, train and validation
-    def mask_test_edges(self, edge_type, type_idx):
+    def mask_test_edges_fail(self, edge_type, type_idx):
         ''' Selects a fraction of the edges of a given adj matrix to be test and val edges
         given the fraction val_test_edges. Also selects an (equal) number of false edges
         from the zero entries of the matrix to be negative test and val edges.
         '''
         positive, _, dims = preprocessing.sparse_to_tuple(self.adj_mats[edge_type][type_idx])
         indexes = [range(k) for k in dims]
-        idx = np.array([ x for x in product(*indexes) ],dtype=int)
+        idx = np.array([ x for x in product(*indexes) ]).astype('int32')
         mask = np.invert((idx[:, None] == positive).all(-1).any(-1))
         negative = idx[mask]
 
@@ -131,7 +129,6 @@ class EdgeMinibatchIterator(object):
         adj_train = sp.csr_matrix(
             (data, (train_edges[:, 0], train_edges[:, 1])),shape=dims)
         self.adj_train[edge_type][type_idx] = self.preprocess_graph(adj_train)
-
         self.train_edges[edge_type][type_idx] = train_edges
         self.train_edges_false[edge_type][type_idx] = train_edges_false
         self.val_edges[edge_type][type_idx] = val_edges
@@ -139,7 +136,7 @@ class EdgeMinibatchIterator(object):
         self.test_edges[edge_type][type_idx] = test_edges
         self.test_edges_false[edge_type][type_idx] = np.array(test_edges_false)
  
-    def mask_test_edges_old(self, edge_type, type_idx):
+    def mask_test_edges(self, edge_type, type_idx):
         ''' Selects a fraction of the edges of a given adj matrix to be test and val edges
         given the fraction val_test_edges. Also selects an (equal) number of false edges
         from the zero entries of the matrix to be negative test and val edges.
@@ -166,7 +163,8 @@ class EdgeMinibatchIterator(object):
         test_edges_false = []
         while len(test_edges_false) < len(test_edges):
             if len(test_edges_false) % 1000 == 0:
-                print("Constructing test edges=", "%04d/%04d" % (len(test_edges_false), len(test_edges)))
+                print("Constructing test edges=", "%04d/%04d" % 
+                      (len(test_edges_false), len(test_edges)))
             idx_i = np.random.randint(0, self.adj_mats[edge_type][type_idx].shape[0])
             idx_j = np.random.randint(0, self.adj_mats[edge_type][type_idx].shape[1])
             if self._ismember([idx_i, idx_j], edges_all): # test that is not a true edge
@@ -179,22 +177,40 @@ class EdgeMinibatchIterator(object):
         val_edges_false = []
         while len(val_edges_false) < len(val_edges):
             if len(val_edges_false) % 1000 == 0:
-                print("Constructing val edges=", "%04d/%04d" % (len(val_edges_false), len(val_edges)))
+                print("Constructing val edges=", "%04d/%04d" % 
+                      (len(val_edges_false), len(val_edges)))
             idx_i = np.random.randint(0, self.adj_mats[edge_type][type_idx].shape[0])
             idx_j = np.random.randint(0, self.adj_mats[edge_type][type_idx].shape[1])
             if self._ismember([idx_i, idx_j], edges_all): # test that is not a true edge
                 continue
             if val_edges_false: # test that is not already in false edges
-                if self._ismember([idx_i, idx_j], val_edges_false):
+                if self._ismember([idx_i, idx_j], val_edges_false) or \
+                   self._ismember([idx_i, idx_j], test_edges_false):
                     continue
             val_edges_false.append([idx_i, idx_j])
+        # Choose false train edges randomly
+        train_edges_false = []
+        while len(train_edges_false) < len(train_edges):
+            if len(train_edges_false) % 1000 == 0:
+                print("Constructing train edges=", "%04d/%04d" % 
+                      (len(train_edges_false), len(train_edges)))
+            idx_i = np.random.randint(0, self.adj_mats[edge_type][type_idx].shape[0])
+            idx_j = np.random.randint(0, self.adj_mats[edge_type][type_idx].shape[1])
+            if self._ismember([idx_i, idx_j], edges_all): # test that is not a true edge
+                continue
+            if train_edges_false: # test that is not already in false edges
+                if self._ismember([idx_i, idx_j], val_edges_false) or\
+                   self._ismember([idx_i, idx_j], test_edges_false) or\
+                   self._ismember([idx_i, idx_j], train_edges_false):
+                    continue
+            train_edges_false.append([idx_i, idx_j])
         # Re-build adj matrices with preprocessing
         data = np.ones(train_edges.shape[0])
         adj_train = sp.csr_matrix(
             (data, (train_edges[:, 0], train_edges[:, 1])),
             shape=self.adj_mats[edge_type][type_idx].shape)
         self.adj_train[edge_type][type_idx] = self.preprocess_graph(adj_train)
-
+        self.train_edges_false[edge_type][type_idx] = train_edges_false
         self.train_edges[edge_type][type_idx] = train_edges
         self.val_edges[edge_type][type_idx] = val_edges
         self.val_edges_false[edge_type][type_idx] = np.array(val_edges_false)

main_decagon.py
@@ -57,6 +57,26 @@ ps= psutil.Process(pid)
 
 # ============================================================================================= #
 # FUNCTIONS
+def sigmoid(x):
+        return 1. / (1 + np.exp(-x))
+def accuracy(edges_pos,edges_neg,pred):
+    """Gives the accuracy of the model given a set of positive and negative entries of 
+    a matrix, and the probability scores of each entry. The method uses np.round to turn
+    probabilities into labels 0 or 1 and feed them to the accuracy_score method of sci-kit
+    learn.
+    """
+    pos_labels = np.ones(np.shape(edges_pos)[0])
+    neg_labels = np.zeros(np.shape(edges_neg)[0])
+    labels = np.hstack((pos_labels,neg_labels))
+    pos_preds=[]
+    scores = np.round(sigmoid(pred))
+    for i,j in edges_pos:
+        pos_preds.append(scores[i,j])
+    neg_preds=[]
+    for i,j in edges_neg:
+        neg_preds.append(scores[i,j])
+    predictions=np.hstack((pos_preds,neg_preds))
+    return metrics.accuracy_score(labels,predictions)
 
 def get_accuracy_scores(edges_pos, edges_neg, edge_type):
     feed_dict.update({placeholders['dropout']: 0})
@@ -65,21 +85,17 @@ def get_accuracy_scores(edges_pos, edges_neg, edge_type):
     feed_dict.update({placeholders['batch_col_edge_type']: edge_type[1]})
     rec = sess.run(opt.predictions, feed_dict=feed_dict)
 
-    def sigmoid(x):
-        return 1. / (1 + np.exp(-x))
-
-    # Predict on test set of edges
+    # Predict on set of edges
     preds = []
-    actual = []
-    predicted = []
+    #actual = []
+    #predicted = []
     edge_ind = 0
     for u, v in edges_pos[edge_type[:2]][edge_type[2]]:
         score = sigmoid(rec[u, v])
         preds.append(score)
         assert adj_mats_orig[edge_type[:2]][edge_type[2]][u,v] == 1, 'Problem 1'
-
-        actual.append(edge_ind)
-        predicted.append((score, edge_ind))
+        #actual.append(edge_ind)
+        #predicted.append((score, edge_ind))
         edge_ind += 1
 
     preds_neg = []
@@ -87,20 +103,18 @@ def get_accuracy_scores(edges_pos, edges_neg, edge_type):
         score = sigmoid(rec[u, v])
         preds_neg.append(score)
         assert adj_mats_orig[edge_type[:2]][edge_type[2]][u,v] == 0, 'Problem 0'
-
-        predicted.append((score, edge_ind))
+        #predicted.append((score, edge_ind))
         edge_ind += 1
 
     preds_all = np.hstack([preds, preds_neg])
     preds_all = np.nan_to_num(preds_all)
     labels_all = np.hstack([np.ones(len(preds)), np.zeros(len(preds_neg))])
-    predicted = list(zip(*sorted(predicted, reverse=True, key=itemgetter(0))))[1]
-
+    #predicted = list(zip(*sorted(predicted, reverse=True, key=itemgetter(0))))[1]
     roc_sc = metrics.roc_auc_score(labels_all, preds_all)
     aupr_sc = metrics.average_precision_score(labels_all, preds_all)
-    apk_sc = rank_metrics.apk(actual, predicted, k=50)
-
-    return roc_sc, aupr_sc, apk_sc
+    #apk_sc = rank_metrics.apk(actual, predicted, k=50)
+    acc = metrics.accuracy_score(labels_all, np.round(preds_all))
+    return roc_sc, aupr_sc, acc
 
 def construct_placeholders(edge_types):
     placeholders = {
@@ -132,12 +146,12 @@ n_se_combo = len(se_combo_name2idx)
 n_se_mono = len(se_mono_name2idx)
 # ============================================================================================= #
 # SETTINGS AND PLACEHOLDERS
-val_test_size = 0.05
+val_test_size = 0.15
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_integer('neg_sample_size', 1, 'Negative sample size.')
 flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
-flags.DEFINE_integer('epochs', 20, 'Number of epochs to train.')
+flags.DEFINE_integer('epochs', 50, 'Number of epochs to train.')
 flags.DEFINE_integer('hidden1', 64, 'Number of units in hidden layer 1.')
 flags.DEFINE_integer('hidden2', 32, 'Number of units in hidden layer 2.')
 flags.DEFINE_float('weight_decay', 0, 'Weight for L2 loss on embedding matrix.')
@@ -147,7 +161,7 @@ flags.DEFINE_integer('batch_size', 512, 'minibatch size.')
 flags.DEFINE_boolean('bias', True, 'Bias term.')
 # Important -- Do not evaluate/print validation performance every iteration as it can take
 # substantial amount of time
-PRINT_PROGRESS_EVERY = 150
+#PRINT_PROGRESS_EVERY = 150
 print("Defining placeholders")
 placeholders = construct_placeholders(edge_types)
 # ============================================================================================= #
@@ -187,15 +201,14 @@ sess.run(tf.global_variables_initializer())
 feed_dict = {}
 # ============================================================================================= #
 # TRAINING
-out_file = 'results_training/TRAIN_'+words[2]+DSE*('_DSE_'+str(n_se_mono))+'_genes_'+\
-            str(n_genes)+'_drugs_'+str(n_drugs)+'_se_'+str(n_se_combo)+'_epochs_'+\
-            str(FLAGS.epochs)+'_h1_'+str(FLAGS.hidden1)+'_h2_'+str(FLAGS.hidden2)+\
-            '_lr_'+str(FLAGS.learning_rate)+'_dropout_'+str(FLAGS.dropout)
-output_data = {}
-acc_scores = np.zeros([num_edge_types,4,1])
+# Metric structures initialization
+validation_metrics = np.zeros([num_edge_types,3,1])
+train_acc = np.zeros([FLAGS.epochs,num_edge_types])
+val_acc = np.zeros([FLAGS.epochs,num_edge_types])
+vm_layer = np.zeros([num_edge_types,3,1])
+# Start training
 print("Train model")
 for epoch in range(FLAGS.epochs):
-    acc_layer = np.zeros([num_edge_types,4,1])
     minibatch.shuffle()
     itr = 0
     edge_count = range(num_edge_types)
@@ -206,57 +219,67 @@ for epoch in range(FLAGS.epochs):
             feed_dict=feed_dict,
             dropout=FLAGS.dropout,
             placeholders=placeholders)
-
         t = time.time()
 
         # Training step: run single weight update
         outs = sess.run([opt.opt_op, opt.cost, opt.batch_edge_type_idx], feed_dict=feed_dict)
         train_cost = outs[1]
         batch_edge_type = outs[2]
-
-        #if itr % PRINT_PROGRESS_EVERY == 0:
+        # Metrics
         if batch_edge_type in edge_count:
-            val_auc, val_auprc, val_apk = get_accuracy_scores(
+            val_auc, val_auprc, val_acs = get_accuracy_scores(
                 minibatch.val_edges, minibatch.val_edges_false,
                 minibatch.idx2edge_type[minibatch.current_edge_type_idx])
             step_time = time.time() - t
-            acc_layer[batch_edge_type,:,0] = [val_auc,val_auprc,val_apk,train_cost]
-            print("Epoch:", "%04d" % (epoch + 1), "Iter:", "%04d" % (itr + 1), "Edge:",
-                  "%04d" % batch_edge_type,
+            vm_layer[batch_edge_type,:,0] = [val_auc,val_auprc,val_acs]
+            print("Epoch:", "%04d" % (epoch + 1), "Iter:", "%04d" % (itr + 1), 
+                  "Edge:", "%04d" % batch_edge_type,
                   "train_loss=", "{:.5f}".format(train_cost),
-                  "val_roc=", "{:.5f}".format(val_auc), "val_auprc=", "{:.5f}".format(val_auprc),
-                  "val_apk=", "{:.5f}".format(val_apk), "time=", "{:.5f}".format(step_time))
+                  "val_roc=", "{:.5f}".format(val_auc),
+                  "val_auprc=", "{:.5f}".format(val_auprc),
+                  "val_acc=", "{:.5f}".format(val_acs),
+                  "time=", "{:.5f}".format(step_time))
             edge_count.remove(batch_edge_type)
-
         itr += 1
-    acc_scores = np.concatenate((acc_scores,acc_layer),axis=2)
-    output_data['val_auc'] = acc_scores[:,0,1:]
-    output_data['val_auprc'] = acc_scores[:,1,1:]
-    output_data['val_apk'] = acc_scores[:,2,1:]
-    output_data['train_cost'] = acc_scores[:,3,1:]
+    # Train accuracy over all train data per epoch
+    for r in range(num_edge_types):
+        i,j,k = minibatch.idx2edge_type[r]
+        true_train_edges = minibatch.train_edges[i,j][k]
+        false_train_edges = minibatch.train_edges_false[i,j][k]
+        true_val_edges = minibatch.val_edges[i,j][k]
+        false_val_edges = minibatch.val_edges_false[i,j][k]
+        feed_dict.update({placeholders['batch_edge_type_idx']:k})
+        feed_dict.update({placeholders['batch_row_edge_type']: i})
+        feed_dict.update({placeholders['batch_col_edge_type']: j})
+        pred = sess.run(opt.predictions,feed_dict=feed_dict)
+        train_acc[epoch,r] = accuracy(true_train_edges,false_train_edges,pred)
+        val_acc[epoch,r] = accuracy(true_val_edges,false_val_edges,pred)
+    validation_metrics = np.concatenate((validation_metrics,vm_layer),axis=2)
+    output_data['val_auc'] = validation_metrics[:,0,1:]
+    output_data['val_auprc'] = validation_metrics[:,1,1:]
+    output_data['train_acc'] = train_acc
+    output_data['val_acc'] = val_acc
     output_data['epoch'] = epoch + 1
-    with open(out_file, 'wb') as f:
+    
+    with open(out_file,'wb') as f:
         pickle.dump(output_data, f, protocol=2)
-    acc_layer = np.zeros([num_edge_types,5,1])
-        
+    vm_layer = np.zeros([num_edge_types,3,1])
+    
+# End of training. Metric structure handling   
 print("Optimization finished!")
-final_scores = np.zeros([num_edge_types,4,1])
+test_scores = np.zeros([num_edge_types,3])
 for et in range(num_edge_types):
-    roc_score, auprc_score, apk_score = get_accuracy_scores(
+    roc_score, auprc_score, acc_score = get_accuracy_scores(
         minibatch.test_edges, minibatch.test_edges_false, minibatch.idx2edge_type[et])
     print("Edge type=", "[%02d, %02d, %02d]" % minibatch.idx2edge_type[et])
     print("Edge type:", "%04d" % et, "Test AUROC score", "{:.5f}".format(roc_score))
     print("Edge type:", "%04d" % et, "Test AUPRC score", "{:.5f}".format(auprc_score))
-    print("Edge type:", "%04d" % et, "Test AP@k score", "{:.5f}".format(apk_score))
+    print("Edge type:", "%04d" % et, "Test Accuracy score", "{:.5f}".format(acc_score))
     print()
-    final_scores[et,0,0] = roc_score
-    final_scores[et,1,0] = auprc_score
-    final_scores[et,2,0] = apk_score
-acc_scores = np.concatenate((acc_scores,final_scores),axis=2)
-output_data['val_auc'] = acc_scores[:,0,1:]
-output_data['val_auprc'] = acc_scores[:,1,1:]
-output_data['val_apk'] = acc_scores[:,2,1:]
-output_data['train_cost'] = acc_scores[:,3,1:]
+    test_scores[et,0] = roc_score
+    test_scores[et,1] = auprc_score
+    test_scores[et,2] = acc_score
+output_data['test_scores'] = test_scores
 memUse = ps.memory_info()
 print('Virtual memory:', memUse.vms)
 print('RSS Memory:', memUse.rss)
@@ -264,6 +287,6 @@ total_time=time.time()-start
 output_data['time'] = total_time
 output_data['vms'] = memUse.vms
 output_data['rss'] = memUse.rss
-print("Total time:",total_time)
-with open(out_file, 'wb') as f:
+with open(out_file,'wb') as f:
     pickle.dump(output_data, f, protocol=2)
+print("Total time:",total_time)