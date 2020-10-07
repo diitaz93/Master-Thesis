Implemented a correct sampling of negative training edges, ReLU in output layer of encoder & no gradient calculation

decagon/deep/inits.py
@@ -19,6 +19,6 @@ def zeros(input_dim, output_dim, name=None):
 
 
 def ones(input_dim, output_dim, name=None):
-    """All zeros."""
+    """All ones."""
     initial = tf.ones((input_dim, output_dim), dtype=tf.float32)
     return tf.Variable(initial, name=name)

decagon/deep/minibatch.py
@@ -180,12 +180,13 @@ class EdgeMinibatchIterator(object):
         feed_dict.update({placeholders['dropout']: dropout})
 
         return feed_dict
-
-    def batch_feed_dict(self, batch_edges, batch_edge_type, placeholders):
+    ## MODIFICATION FOR NEGATIVE TRAINING ##
+    def batch_feed_dict(self, batch_edges, neg_batch_edges, batch_edge_type, placeholders):
         """ Feed the tensorflow feed dictionary with the edges and indices taken as parameters
         """
         feed_dict = dict()
         feed_dict.update({placeholders['batch']: batch_edges})
+        feed_dict.update({placeholders['neg_batch']: neg_batch_edges})
         feed_dict.update({placeholders['batch_edge_type_idx']: batch_edge_type})
         feed_dict.update({placeholders['batch_row_edge_type']: self.idx2edge_type[batch_edge_type][0]}) # 0 or 1, location of row in dictionary
         feed_dict.update({placeholders['batch_col_edge_type']: self.idx2edge_type[batch_edge_type][1]}) # 0 or 1, location of col in dictionary
@@ -231,27 +232,31 @@ class EdgeMinibatchIterator(object):
         start = self.batch_num[self.current_edge_type_idx] * self.batch_size
         self.batch_num[self.current_edge_type_idx] += 1
         batch_edges = self.train_edges[i,j][k][start: start + self.batch_size]
-        return self.batch_feed_dict(batch_edges, self.current_edge_type_idx, placeholders)
+        ## MODIFICATION FOR NEGATIVE TRAINING ##
+        neg_batch_edges = self.train_edges_false[i,j][k][start: start + self.batch_size]
+        return self.batch_feed_dict(batch_edges, neg_batch_edges, self.current_edge_type_idx, placeholders)
 
     def num_training_batches(self, edge_type, type_idx):
         """ Returns the total number of different batches that can be possible formed
         with the number of edges in training given the edge type and the index.
         """
         return len(self.train_edges[edge_type][type_idx]) // self.batch_size + 1
-
+    ## MODIFICATION FOR NEGATIVE VALIDATION ##
     def val_feed_dict(self, edge_type, type_idx, placeholders, size=None):
         """ Feed the tensorflow feed dictionary with validation edges instead of training
         edges given the edge type and the index. A size of the dataset (number of edges)
         can be passed as an optional parameter.
         """
         edge_list = self.val_edges[edge_type][type_idx]
+        neg_edge_list = self.val_edges_false[edge_type][type_idx]
         if size is None:
-            return self.batch_feed_dict(edge_list, edge_type, placeholders)
+            return self.batch_feed_dict(edge_list, neg_edge_list, edge_type, placeholders)
         else:
             ind = np.random.permutation(len(edge_list))
             val_edges = [edge_list[i] for i in ind[:min(size, len(ind))]]
-            return self.batch_feed_dict(val_edges, edge_type, placeholders)
-
+            neg_val_edges = [neg_edge_list[i] for i in ind[:min(size, len(ind))]]
+            return self.batch_feed_dict(val_edges, neg_val_edges, edge_type, placeholders)
+        ## MODIFICATION FOR NEGATIVE TRAINING ##
     def shuffle(self):
         """ Re-shuffle the training set for all edge types.
             Also reset the batch number and the free batch availiable.
@@ -259,6 +264,7 @@ class EdgeMinibatchIterator(object):
         for edge_type in self.edge_types:
             for k in range(self.edge_types[edge_type]):
                 self.train_edges[edge_type][k] = np.random.permutation(self.train_edges[edge_type][k])
+                self.train_edges_false[edge_type][k] = np.random.permutation(self.train_edges_false[edge_type][k])
                 self.batch_num[self.edge_type2idx[edge_type[0], edge_type[1], k]] = 0
         self.current_edge_type_idx = 0
         self.freebatch_edge_types = list(range(self.num_edge_types))

decagon/deep/model.py
@@ -15,9 +15,7 @@ class Model(object):
         
         for kwarg in kwargs.keys():
             assert kwarg in allowed_kwargs, 'Invalid keyword argument: ' + kwarg
-        # Duplicado?
-        #for kwarg in kwargs.keys():
-            #assert kwarg in allowed_kwargs, 'Invalid keyword argument: ' + kwarg
+            
         name = kwargs.get('name')
         if not name:
             name = self.__class__.__name__.lower()
@@ -39,18 +37,6 @@ class Model(object):
         # This makes the variables created in the model availiable for updating and training
         variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=self.name)
         self.vars = {var.name: var for var in variables}
-
-    def fit(self):
-        """ Ignore, probably meant for another model. The equivalent for DECAGON is implemented in
-        optimization.
-        """
-        pass
-
-    def predict(self):
-        """ Ignore, probably meant for another model. The equivalent for DECAGON is implemented in
-        optimization.
-        """
-        pass
 
 
 class DecagonModel(Model):
@@ -96,8 +82,8 @@ class DecagonModel(Model):
         self.embeddings = [None] * self.num_obj_types
         for i, embeds in self.embeddings_reltyp.items():
             # Why is this commented? No activation function in the 2nd layer -Sebastian 
-            # self.embeddings[i] = tf.nn.relu(tf.add_n(embeds)) -SNAP
-            self.embeddings[i] = tf.add_n(embeds)
+            self.embeddings[i] = tf.nn.relu(tf.add_n(embeds))
+            #self.embeddings[i] = tf.add_n(embeds)
             
         # DECODER
         self.edge_type2decoder = {}
@@ -125,7 +111,7 @@ class DecagonModel(Model):
                     act=lambda x: x, dropout=self.dropout)
             else:
                 raise ValueError('Unknown decoder type')
-# Could not these for loops be merged in one? Is the one above necessary? It is never used again
+            
         self.latent_inters = []
         self.latent_varies = []
         for edge_type in self.edge_types:

decagon/deep/optimizer.py
@@ -22,39 +22,25 @@ class DecagonOptimizer(object):
         self.batch_size = batch_size
 
         self.inputs = placeholders['batch']
+        self.neg_inputs = placeholders['neg_batch']
         self.batch_edge_type_idx = placeholders['batch_edge_type_idx']
         self.batch_row_edge_type = placeholders['batch_row_edge_type']
         self.batch_col_edge_type = placeholders['batch_col_edge_type']
 
         self.row_inputs = tf.squeeze(gather_cols(self.inputs, [0]))
         self.col_inputs = tf.squeeze(gather_cols(self.inputs, [1]))
+        self.neg_rows = tf.squeeze(gather_cols(self.neg_inputs, [0]))
+        self.neg_cols = tf.squeeze(gather_cols(self.neg_inputs, [1]))
         # Indices for selecting the correct (drug or gene) embeddings in the predict functions
         obj_type_n = [self.obj_type2n[i] for i in range(len(self.embeddings))] #[n_genes,n_drugs]
         self.obj_type_lookup_start = tf.cumsum([0] + obj_type_n[:-1])#[0,n_genes]
         self.obj_type_lookup_end = tf.cumsum(obj_type_n)#[n_genes,n_drugs+n_genes]
         
-        # Sample negative interactions
-        # sample row indices (labels) per training example with a probability proportional to degree
-        labels = tf.reshape(tf.cast(self.row_inputs, dtype=tf.int64), [self.batch_size, 1])#rowsinp
-        neg_samples_list = []
-        for i, j in self.edge_types:
-            for k in range(self.edge_types[i,j]):
-                neg_samples, _, _ = tf.nn.fixed_unigram_candidate_sampler(
-                    true_classes=labels,
-                    num_true=1,
-                    num_sampled=self.batch_size, #length of output
-                    unique=False,
-                    range_max=len(self.degrees[i][k]), # number of nodes, range for sampling
-                    distortion=0.75,
-                    unigrams=self.degrees[i][k].tolist()) # higher degree more probability
-                neg_samples_list.append(neg_samples)
-        self.neg_samples = tf.gather(neg_samples_list, self.batch_edge_type_idx)
-        
         self.preds = self.batch_predict(self.row_inputs, self.col_inputs)
         self.outputs = tf.diag_part(self.preds)
         self.outputs = tf.reshape(self.outputs, [-1])
 
-        self.neg_preds = self.batch_predict(self.neg_samples, self.col_inputs)
+        self.neg_preds = self.batch_predict(self.neg_rows, self.neg_cols)
         self.neg_outputs = tf.diag_part(self.neg_preds)
         self.neg_outputs = tf.reshape(self.neg_outputs, [-1])
 
@@ -121,19 +107,10 @@ class DecagonOptimizer(object):
         self.predictions = tf.matmul(product3, tf.transpose(col_embeds))
 
     def _build(self):
-        #self.cost = self._hinge_loss(self.outputs, self.neg_outputs)
         self.cost = self._xent_loss(self.outputs, self.neg_outputs)
         self.optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
         #  TO SOLVE (by SNAPS): This function makes the dense tensor
         self.opt_op = self.optimizer.minimize(self.cost)
-        # Comment the following calculation of gradients as it is not needed yet -Seb
-        #self.grads_vars = self.optimizer.compute_gradients(self.cost)
-
-    def _hinge_loss(self, aff, neg_aff):
-        """Maximum-margin optimization using the hinge loss."""
-        diff = tf.nn.relu(tf.subtract(neg_aff, tf.expand_dims(aff, 0) - self.margin), name='diff')
-        loss = tf.reduce_sum(diff)
-        return loss
 
     def _xent_loss(self, aff, neg_aff):
         """Cross-entropy optimization."""